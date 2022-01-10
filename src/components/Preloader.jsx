4cd928798d6aecb2593b6d8a0bac4b5524919cff
Bambietta <bambi666@example.com>
2022-01-10 19:29:59 +0200

    project complete

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,7 @@
+function Preloader() {
+    return <div className="progress deep-purple lighten-5">
+        <div className="indeterminate deep-purple lighten-1"></div>
+    </div>
+}
+
+export {Preloader}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="deep-purple lighten-2">
+        <div className="nav-wrapper">
+        <a href="#" className="brand-logo deep-purple-text text-lighten-5">Movies DB</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="!#">Repo</a></li>
+        </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
