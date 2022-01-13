c540926b156a1a47d4bbd5466ad94e73250469a3
Bambietta <bambi666@example.com>
2022-01-13 23:51:07 +0200

    repo links added

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,19 @@
-function Footer () {
-    return <footer className="page-footer deep-purple lighten-3 ">
-        <div className="footer-copyright">
-        <div className="container deep-purple-text text-lighten-5">
-        © {new Date().getFullYear()} Copyright Text
-        <a className="deep-purple-text text-lighten-5 right" href="#!">Repo</a>
-        </div>
-        </div>
-    </footer>
+function Footer() {
+    return (
+        <footer className='page-footer deep-purple lighten-3 '>
+            <div className='footer-copyright'>
+                <div className='container deep-purple-text text-lighten-5'>
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className='deep-purple-text text-lighten-5 right'
+                        href='https://github.com/Esde4th/react-movies-project'
+                    >
+                        Repo
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
-export {Footer}
+export { Footer };
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,23 @@
 function Header() {
-    return <nav className="deep-purple lighten-2">
-        <div className="nav-wrapper">
-        <a href="#" className="brand-logo deep-purple-text text-lighten-5">Movies DB</a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
-        </ul>
-        </div>
-    </nav>
+    return (
+        <nav className='deep-purple lighten-2'>
+            <div className='nav-wrapper'>
+                <a
+                    href='#'
+                    className='brand-logo deep-purple-text text-lighten-5'
+                >
+                    Movies DB
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a href='https://github.com/Esde4th/react-movies-project'>
+                            Repo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
-export {Header}
+export { Header };
